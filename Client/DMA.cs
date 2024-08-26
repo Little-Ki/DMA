f0482d484764958bc5306835ba3386c08f7fbe91
Little-Ki <970559705@qq.com>
2024-08-26 21:32:47 +0800

    update

---
+++ Client/DMA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class DMA
+    {
+        private UInt32 id;
+
+        DMA()
+        {
+            id = DMALibrary.CreateDMA();
+        }
+
+        public bool Initialize()
+        {
+            return DMALibrary.InitializeDMA(id);
+        }
+
+        public void Close()
+        {
+            DMALibrary.CloseDMA(id);
+        }
+
+        public bool OpenProcess(string processName)
+        {
+            return DMALibrary.OpenProcess(id, processName);
+        }
+
+        public float ReadFloat(IntPtr address)
+        {
+            return DMALibrary.ReadFloat(id, address);
+        }
+        public bool WriteFloat(IntPtr address, float value)
+        {
+            return DMALibrary.WriteFloat(id, address, value);
+        }
+
+        public int ReadInt(IntPtr address)
+        {
+            return DMALibrary.ReadInt(id, address);
+        }
+
+        public bool WriteInt(IntPtr address, int value)
+        {
+            return DMALibrary.WriteInt(id, address, value);
+        }
+    }
+}
--- Library/exports.h
+++ Library/exports.h
@@ -4,6 +4,19 @@
 
 #define DLL_EXPORT extern "C" __declspec(dllexport)
 
-DLL_EXPORT uint32_t createDMA();
+DLL_EXPORT uint32_t CreateDMA();
 
-DLL_EXPORT bool initializeDMA(uint32_t id);
+DLL_EXPORT bool InitializeDMA(uint32_t dma_id);
+
+DLL_EXPORT void CloseDMA(uint32_t dma_id);
+
+DLL_EXPORT bool OpenProcess(uint32_t dma_id, const wchar_t* wname);
+
+DLL_EXPORT float ReadFloat(uint32_t dma_id, uintptr_t address);
+
+DLL_EXPORT bool WriteFloat(uint32_t dma_id, uintptr_t address, float value);
+
+DLL_EXPORT int ReadInt(uint32_t dma_id, uintptr_t address);
+
+DLL_EXPORT bool WriteInt(uint32_t dma_id, uintptr_t address, int value);
+
